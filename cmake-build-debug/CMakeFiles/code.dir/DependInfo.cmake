c5c6a48bcdac69a7d3809efa2083cdb961b84f33
jinhongyi <3231950289@qq.com>
2018-11-28 17:52:00 +0800

    haha

---
+++ cmake-build-debug/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jinho/CLionProjects/BASIC/Basic.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/Basic.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/calculate.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/calculate.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/error.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/error.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/evalstate.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/evalstate.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/exp.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/exp.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/parser.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/parser.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/program.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/program.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/simpio.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/simpio.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/statement.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/statement.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/strlib.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/strlib.cpp.obj"
+  "C:/Users/jinho/CLionProjects/BASIC/tokenscanner.cpp" "C:/Users/jinho/CLionProjects/BASIC/cmake-build-debug/CMakeFiles/code.dir/tokenscanner.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/Basic.cpp.obj"
+  "CMakeFiles/code.dir/evalstate.cpp.obj"
+  "CMakeFiles/code.dir/exp.cpp.obj"
+  "CMakeFiles/code.dir/parser.cpp.obj"
+  "CMakeFiles/code.dir/program.cpp.obj"
+  "CMakeFiles/code.dir/statement.cpp.obj"
+  "CMakeFiles/code.dir/error.cpp.obj"
+  "CMakeFiles/code.dir/tokenscanner.cpp.obj"
+  "CMakeFiles/code.dir/simpio.cpp.obj"
+  "CMakeFiles/code.dir/strlib.cpp.obj"
+  "CMakeFiles/code.dir/calculate.cpp.obj"
+  "code.pdb"
+  "code.exe"
+  "code.exe.manifest"
+  "libcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

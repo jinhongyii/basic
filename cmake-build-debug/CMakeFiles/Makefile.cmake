7c93a499e2a668d88f034295d8b5b2405bf5adf1
jinhongyi <3231950289@qq.com>
2018-11-28 17:43:16 +0800

    change exe name to code

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/BASIC.dir/DependInfo.cmake"
+  "CMakeFiles/code.dir/DependInfo.cmake"
   )
